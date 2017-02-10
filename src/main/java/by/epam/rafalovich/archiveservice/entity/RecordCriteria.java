cde35f5a92c6e8710a9d0790c2f99b8cd13a3337
Dzmitry Rafalovich <dzmitry_rafalovich@epam.com>
2017-02-10 20:32:36 +0300

    change wsdl and => generated classe from jaxb

---
+++ src/main/java/by/epam/rafalovich/archiveservice/entity/RecordCriteria.java
@@ -0,0 +1,7 @@
+package by.epam.rafalovich.archiveservice.entity;
+
+/**
+ * Created by Dzmitry_Rafalovich on 2/10/2017.
+ */
+public class RecordCriteria {
+}
---
+++ src/main/java/by/epam/rafalovich/archiveservice/provider/soap/impl/NewArchiveService.java
@@ -0,0 +1,7 @@
+package by.epam.rafalovich.archiveservice.provider.soap.impl;
+
+/**
+ * Created by Dzmitry_Rafalovich on 2/10/2017.
+ */
+public class NewArchiveService {
+}
