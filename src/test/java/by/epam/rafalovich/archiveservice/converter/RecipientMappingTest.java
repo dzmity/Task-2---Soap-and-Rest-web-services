23edeaf8d5656743a425bfa712e714d6392d7ec0
drafalovich <d.rafalovich@gmail.com>
2017-02-12 19:54:23 +0300

    change recipientMappingTest

--- src/test/java/by/epam/rafalovich/archiveservice/converter/RecipientMappingTest.java
+++ src/test/java/by/epam/rafalovich/archiveservice/converter/RecipientMappingTest.java
@@ -1,8 +1,11 @@
 package by.epam.rafalovich.archiveservice.converter;
 
-import by.epam.rafalovich.archiveservice.ChannelType;
-import by.epam.rafalovich.archiveservice.Recipient;
+
+import by.epam.rafalovich.archiveservice.CommunicationChannelType;
+
+import by.epam.rafalovich.archiveservice.RecipientType;
 import by.epam.rafalovich.archiveservice.entity.CommunicationChannel;
+import by.epam.rafalovich.archiveservice.entity.Recipient;
 import org.dozer.DozerBeanMapper;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -29,42 +32,42 @@ public class RecipientMappingTest {
     }
 
     @Test
-    public void testRecipientMarshalling() {
+    public void testRecipientTypeMapping() {
 
         long id = 10L;
         String contact = "+375336012781";
-        ChannelType type = ChannelType.SMS;
+        CommunicationChannelType type = CommunicationChannelType.SMS;
         CommunicationChannel channel = CommunicationChannel.SMS;
 
-        Recipient dto = new Recipient();
+        RecipientType dto = new RecipientType();
         dto.setId(BigInteger.valueOf(id));
         dto.setRecipientContact(contact);
         dto.setChannelType(type);
 
-        by.epam.rafalovich.archiveservice.entity.Recipient domen = new by.epam.rafalovich.archiveservice.entity.Recipient();
-        domen.setRecipientId(id);
-        domen.setRecipientContact(contact);
-        domen.setChanel(channel);
+        Recipient domain = new Recipient();
+        domain.setRecipientId(id);
+        domain.setRecipientContact(contact);
+        domain.setChanel(channel);
 
-        by.epam.rafalovich.archiveservice.entity.Recipient result = mapper.map(dto, by.epam.rafalovich.archiveservice.entity.Recipient.class);
+        Recipient result = mapper.map(dto, Recipient.class);
 
-        assertEquals(result, domen);
+        assertEquals(result, domain);
     }
 
     @Test
-    public void testRecipientUnmarshalling() {
+    public void testRecipientMapping() {
 
         long id = 5L;
         String contact = "qwerty@gmail.com";
-        ChannelType type = ChannelType.EMAIL;
+        CommunicationChannelType type = CommunicationChannelType.EMAIL;
         CommunicationChannel channel = CommunicationChannel.EMAIL;
 
-        by.epam.rafalovich.archiveservice.entity.Recipient domen = new by.epam.rafalovich.archiveservice.entity.Recipient();
-        domen.setRecipientId(id);
-        domen.setRecipientContact(contact);
-        domen.setChanel(channel);
+        Recipient domain = new Recipient();
+        domain.setRecipientId(id);
+        domain.setRecipientContact(contact);
+        domain.setChanel(channel);
 
-        by.epam.rafalovich.archiveservice.Recipient result = mapper.map(domen, by.epam.rafalovich.archiveservice.Recipient.class);
+        RecipientType result = mapper.map(domain, RecipientType.class);
 
         assertPropertyLenientEquals("id", BigInteger.valueOf(id), result);
         assertPropertyLenientEquals("recipientContact", contact, result);
