8a8025ce7337052a2c2294183b37014c7e9d9a9f
Dzmitry Rafalovich <dzmitry_rafalovich@epam.com>
2017-02-15 10:08:42 +0300

    change search criteria from between dateTime to less than and greater than

--- src/main/java/by/epam/rafalovich/archiveservice/dao/impl/RecordDAOImpl.java
+++ src/main/java/by/epam/rafalovich/archiveservice/dao/impl/RecordDAOImpl.java
@@ -21,11 +21,6 @@ import java.util.Collection;
 @Transactional
 public class RecordDAOImpl extends GenericDAOImpl<CommunicationRecord> implements RecordDAO {
 
-    private static final int START_YEAR = 2015;
-    private static final int START_MONTH = 1;
-    private static final int START_DAY = 1;
-    private static final int START_HOUR= 0;
-    private static final int START_MINUTE= 0;
     private static final String SENDER_ID_PROPERTY_NAME = "sender.senderId";
     private static final String RECIPIENT_ID_PROPERTY_NAME = "recipient.recipientId";
     private static final String OPERATION_TYPE_PROPERTY_NAME = "operationType";
@@ -41,15 +36,12 @@ public class RecordDAOImpl extends GenericDAOImpl<CommunicationRecord> implement
         LocalDateTime endDateTime = recordCriteria.getEndDateTime();
         Operation type = recordCriteria.getOperationType();
 
-        LocalDateTime start = startDateTime != null ? startDateTime : LocalDateTime.of(
-                START_YEAR, START_MONTH, START_DAY, START_HOUR, START_MINUTE);
-        LocalDateTime end = endDateTime != null ? endDateTime : LocalDateTime.now();
-
         Criteria criteria = currentSession().createCriteria(CommunicationRecord.class);
         criteria = senderId != null ? criteria.add(Restrictions.eq(SENDER_ID_PROPERTY_NAME, senderId)) : criteria;
         criteria = recipientId != null ? criteria.add(Restrictions.eq(RECIPIENT_ID_PROPERTY_NAME, recipientId)) : criteria;
         criteria = type != null ? criteria.add(Restrictions.eq(OPERATION_TYPE_PROPERTY_NAME, type)) : criteria;
-        criteria.add(Restrictions.between(DATE_TIME_PROPERTY_NAME, start,end));
+        criteria = startDateTime != null ? criteria.add(Restrictions.ge(DATE_TIME_PROPERTY_NAME, startDateTime)) : criteria;
+        criteria = endDateTime != null ? criteria.add(Restrictions.le(DATE_TIME_PROPERTY_NAME, endDateTime)) : criteria;
 
         return criteria.list();
     }
